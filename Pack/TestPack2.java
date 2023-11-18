48d6cdef3df633b1398ef5c3c23093bbf5102563
Vibha <65108188+Er-Vibha@users.noreply.github.com>
2023-11-18 19:44:35 +0530

    Package example

---
+++ Pack/TestPack2.java
@@ -0,0 +1,10 @@
+package Pack;
+import myPack.TestPack1;
+import myPack.TestPack1;
+
+class TestPack2 {
+    public static void main(String[] args) {
+     TestPack1 ob=new TestPack1();
+     ob.msg();
+    }
+}
---
+++ myPack/TestPack1.java
@@ -0,0 +1,4 @@
+package myPack;
+public class TestPack1 {
+   public void msg(){ System.out.print("My Pack");}
+}
